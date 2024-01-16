8e2c2e1d634c9df881e01b7734b2566628805e68
Vinisha0306 <lathiyavinisha03@gmail.com>
2024-01-16 10:38:53 +0530

    16/01/2024

--- ch-4_2/4_2-1.dart
+++ ch-4_2/4_2-1.dart
@@ -2,13 +2,20 @@ import 'dart:io';
 
 class Student
 {
-
+  int rollno=0;
+  String name="no";
 }
 
 void main()
 {
-  stdout.write("Enter number of students:");
-  int n = int.parse(stdin.readLineSync() ?? "0");
-
   Student mylist=Student();
+
+  stdout.write("Enter roll no:");
+  mylist.rollno = int.parse(stdin.readLineSync() ?? "0");
+
+  stdout.write("Enter Name:");
+  mylist.name = stdin.readLineSync() ?? "name";
+
+  print("Roll no: ${mylist.rollno}");
+  print("Name : ${mylist.name}");
 }
