14ffa462e6ee2cef32dc9798ebb11186805cdb5c
Vinisha0306 <lathiyavinisha03@gmail.com>
2024-01-24 09:37:10 +0530

    24/01/2024

---
+++ pr-3/pr-3_1.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+int Allsum({required List<int> mylist})
+{
+  int sum=0;
+  for(int i=0;i<mylist.length;i++)
+  {
+    sum += mylist[i];
+  }
+
+  return sum;
+}
+
+void main()
+{
+  stdout.write("Enter number of List:");
+  int length = int.parse(stdin.readLineSync() ?? "0");
+  List<int> mylist = List.generate(length,(index){
+stdout.write("Enter ${index+1}:");
+  return int.parse(stdin.readLineSync() ?? "0");});
+
+  print("sum of all List elements:${Allsum(mylist:mylist)}");
+}
